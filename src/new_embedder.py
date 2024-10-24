3c66cd921347fc50221ef349416a87a6d3750c8b
trankhiem <trankhiem2003@gmail.com>
2024-10-24 12:52:40 +0700

    fixbug023

--- src/new_embedder.py
+++ src/new_embedder.py
@@ -634,7 +634,7 @@ def get_linear_tgt_model(args, root, sample_shape, num_classes, loss,lora_rank =
             src_ys.append(y_.detach().cpu())
             src_feats.append(out.detach().cpu())
     src_feats = torch.cat(src_feats, 0)
-    src_ys = torch.cat(src_ys, 0).long()
+    src_ys = torch.cat(src_ys, 0)
     src_train_dataset = torch.utils.data.TensorDataset(src_feats, src_ys)        
     del src_model, src_train_loader    
 
