3d7e3c83daf1ad163a6611ef6b9c75d06adb4bfa
khiembk <trankhiem2003@gmail.com>
2025-01-21 07:23:22 +0000

    update(fix):updateInstallPeft

--- src/startup-hook.sh
+++ src/startup-hook.sh
@@ -1,11 +1,12 @@
 CURPATH=`pwd`
 
 # OTDD 
+pip install peft
 pip install gdown
 cd otdd
 pip install -r requirements.txt
 pip install .
-pip install torch-scatter torch-spars -f https://data.pyg.org/whl/torch-2.4.1%2Bcu124.html
+#pip install torch-scatter torch-spars -f https://data.pyg.org/whl/torch-2.4.1%2Bcu124.html
 # for FSD50K
 # pip install librosa
 # apt-get install -y libsndfile1
